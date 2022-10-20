7f3ad02326e298e5a00fcacda4be1427e32c0688
creasedigital <ojorechris@gmail.com>
2022-10-20 13:02:48 +0100

    accepting user input

---
+++ C/get_user_input.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int age;
+
+    printf("Enter your age: ");
+
+    scanf("%d", &age);
+
+    printf("Darren is %d years old", age);
+
+    return (0);
+}
