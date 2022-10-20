52c5df361f22e0066daf1d6143e1b0efc9faa6dc
creasedigital <ojorechris@gmail.com>
2022-10-20 13:14:30 +0100

    more input methods

--- C/get_user_input.c
+++ C/get_user_input.c
@@ -9,7 +9,31 @@ int main()
 
     scanf("%d", &age);
 
-    printf("Darren is %d years old", age);
+    printf("Darren is %d years old\n", age);
+
+    double gpa;
+
+    printf("Enter your grade point average: ");
+
+    scanf("%lf", &gpa);
+
+    printf("Your current grade is %f\n ", gpa);
+
+    char name[20];
+
+    printf("Enter your name: ");
+
+    scanf("%s", name);
+
+    printf("Your name is %s\n ", name);
+
+    char fname[20];
+
+    printf("Enter your first name: ");
+
+    fgets(fname, 20, stdin);
+
+    printf("Your name is %s\n ", name);
 
     return (0);
 }
