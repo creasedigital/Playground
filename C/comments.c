1d8d9c84139594cbc60561fae4e7321be784fc1b
creasedigital <ojorechris@gmail.com>
2022-10-20 12:46:17 +0100

    comments are fun

---
+++ C/comments.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+
+ int age = 40;
+ double pi = 3.142;
+ float gpa = 3.92;
+
+ /**
+  * codes in this block won't work
+  * printf("The operation has a value of %f\n", (pi + gpa) * 9);
+  * printf("%f\n", pow(4, 2));
+  * printf("%f\n", sqrt(64));
+  * printf("%f\n", ceil(32.75));
+  * printf("%f", floor(44.75));
+  */
+ return (0);
+}
