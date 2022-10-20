7521a90fbd40aea897bb9ab6011956ce17f292b7
creasedigital <ojorechris@gmail.com>
2022-10-20 12:52:43 +0100

    constants in C

---
+++ C/constants.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    const int NUM = 0;
+    const int FAV_NUM = 4;
+
+    printf("%d\n", NUM);
+    printf("%d\n", FAV_NUM);
+
+    return (0);
+}
