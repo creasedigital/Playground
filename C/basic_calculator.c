4dd2b7b0ea426a2b1a5e1c6d812e7a02bf79026c
creasedigital <ojorechris@gmail.com>
2022-10-21 11:50:04 +0100

    addition method

---
+++ C/basic_calculator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // addition
+
+    int num1;
+    int num2;
+
+    printf("Enter first number: ");
+
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+
+    scanf("%d", &num2);
+
+    printf("Answer: %d", num1 + num2);
+
+    return (0);
+}
