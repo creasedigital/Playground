b239233504552382abb524489e1bdba8e2624d12
creasedigital <ojorechris@gmail.com>
2022-10-22 20:46:28 +0100

    physical memory address

--- C/memory.c
+++ C/memory.c
@@ -4,6 +4,12 @@
 int main()
 
 {
+    int age = 30;
+    double gpa = 3.4;
+    char grade = 'A';
+
+
+    printf("%p\n", &age);
 
     return (0);
 }
--- C/pointers.c
+++ C/pointers.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 
 int main()
-
 {
+
+
 
     return (0);
 }
