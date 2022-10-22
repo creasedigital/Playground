6f7d7d631de84395e659839532414a004748d8ff
creasedigital <ojorechris@gmail.com>
2022-10-22 17:13:11 +0100

    switch statement

--- C/switch.c
+++ C/switch.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    char grade = 'G';
+
+    switch(grade)
+    {
+    case 'A' :
+        printf("You did excellently well");
+        break;
+    case 'B' :
+        printf("You did fairly well");
+        break;
+    case 'C' :
+        printf("You did averagely");
+        break;
+    case 'D' :
+        printf("You did bad");
+        break;
+    case 'E' :
+        printf("You did poorly");
+        break;
+    default :
+        printf("You scored an invalid grade");
+    }
+
+    return (0);
+}
