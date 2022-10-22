e6f0a9eefe5c84e9cf23655fb7bcff81bbf82384
creasedigital <ojorechris@gmail.com>
2022-10-22 17:03:54 +0100

    calculator bug fix

--- C/calc2.c
+++ C/calc2.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main ()
+{
+    double num1;
+    double num2;
+    char op;
+
+    printf("Enter a number: \n");
+    scanf("%lf", &num1);
+    printf("Enter an operator: \n");
+    scanf(" %c", &op);
+    printf("Enter a number: ");
+    scanf("%lf", &num2);
+
+    if (op == '+')
+    {
+        printf("%f", num1 + num2);
+    } else if (op == '-')
+    {
+        printf("%f", num1 - num2);
+    } else if (op == '/')
+    {
+        printf("%f", num1 / num2);
+    } else if (op == '*' || op == 'x' || op == 'X')
+    {
+        printf("%f", num1 * num2);
+    } else
+    {
+        printf("INVALID OPERATOR! ");
+    }
+
+    return (0);
+}
