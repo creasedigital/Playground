d12895461525e0d425a3e83edcb2903ef886d5df
creasedigital <ojorechris@gmail.com>
2022-10-22 18:09:43 +0100

    guess game

--- C/guessing_game.c
+++ C/guessing_game.c
@@ -4,6 +4,19 @@
 int main()
 
 {
+    int secretNumber = 5;
+    int guess;
+
+    while(guess != secretNumber)
+    {
+        printf("Enter the secret number: ");
+        scanf("%d", &guess);
+        if(guess != secretNumber)
+            printf("please try again\n");
+        else
+            return printf("Your guess was as good as mine!");
+    }
+
 
     return (0);
 }
