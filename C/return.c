a8530521dda4810b897a58c501255ec003295acc
creasedigital <ojorechris@gmail.com>
2022-10-22 16:22:51 +0100

    'return'

--- C/return.c
+++ C/return.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * any function that returns a type should be placed above main
+ */
+
+ double cube (double num)
+ {
+     double result = num * num * num;
+     return result;
+ }
+
+int main ()
+{
+    printf("Answer: %f", cube(4.8));
+    return (0);
+}
