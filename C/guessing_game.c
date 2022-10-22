2ff443c38ed84169373411a0ed823352656eeef9
creasedigital <ojorechris@gmail.com>
2022-10-22 18:49:36 +0100

    more complicated logic to guess

--- C/guessing_game.c
+++ C/guessing_game.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(){
 
-{
     int secretNumber = 5;
     int guess;
+    int guessCount = 0;
+    int guessLimit = 3;
+    int outOfGuesses = 0;
 
-    while(guess != secretNumber)
-    {
-        printf("Enter the secret number: ");
-        scanf("%d", &guess);
-        if(guess != secretNumber)
-            printf("please try again\n");
-        else
-            return printf("Your guess was as good as mine!");
+    while(guess != secretNumber && outOfGuesses == 0) {
+        if(guessCount < guessLimit){
+            printf("Enter the secret number: ");
+            scanf("%d", &guess);
+            guessCount++;
+            } else {
+                outOfGuesses = 1;
+            }
     }
+     if(outOfGuesses == 1){
+        printf("you ran out of guesses\n");
+    } else {
+        printf("Your guess was as good as mine!");
+        }
+
+
 
 
     return (0);
