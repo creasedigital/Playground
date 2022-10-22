89f5b84d0b539bf139a8046f08dcb13dcffac469
creasedigital <ojorechris@gmail.com>
2022-10-22 20:59:45 +0100

    creating pointers

--- C/pointers.c
+++ C/pointers.c
@@ -4,7 +4,15 @@
 int main()
 {
 
+    int age = 30;
+    int * pAge = &age;
+    double gpa = 3.4;
+    double * pGpa = &gpa;
+    char grade = 'A';
+    char * aGrade = &grade;
 
+
+    printf("age's memory address: %p\n", &age);
 
     return (0);
 }
