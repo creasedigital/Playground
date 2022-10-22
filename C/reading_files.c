853903704b2d6b21f4bab8f9a8aa5ee5ebc830e2
creasedigital <ojorechris@gmail.com>
2022-10-22 21:32:50 +0100

    using fgets to read file lines

--- C/reading_files.c
+++ C/reading_files.c
@@ -2,8 +2,18 @@
 #include <stdlib.h>
 
 int main()
-
 {
+    char line[255];
 
+    FILE * fpointer = fopen("employees.txt", "r");
+
+    fgets(line, 255, fpointer);
+    fgets(line, 255, fpointer);
+    fgets(line, 255, fpointer);
+    fgets(line, 255, fpointer);
+
+    printf("%s", line);
+
+    fclose(fpointer);
     return (0);
 }
